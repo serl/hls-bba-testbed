408f62f8aca655cd5f9dba2ebe7a4987c23ac358
Sergio Livi <livi@i3s.unice.fr>
2015-06-09 19:24:48 +0200

    propagate exit code

--- run_schedule.sh
+++ run_schedule.sh
@@ -2,13 +2,16 @@
 
 source code/colors.sh
 
+exitcode=0
 for schedule_file in "$@"; do
 	if [ -d $schedule_file ]; then
 		schedule_file="$schedule_file/jobs.sched"
 	fi
 	echo -e "${IBlue}Running $schedule_file...${Color_Off}"
 	vagrant ssh server --command '/vagrant/code/master_scheduler.sh' -- -T < $schedule_file
+	exitcode=$?
 	sleep 1
 	echo
 done
+exit $exitcode
 
