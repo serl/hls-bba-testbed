aeb2b2116d7b4fc0540841ad7f5c9b71bf367bf7
Sergio Livi <livi@i3s.unice.fr>
2015-06-25 20:07:56 +0200

    should be faster

--- code/tcpprobe_helper.sh
+++ code/tcpprobe_helper.sh
@@ -5,9 +5,11 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 
+config_hz=$(grep 'CONFIG_HZ=' /boot/config-$(uname -r))
+
 sudo rmmod tcp_probe 2>/dev/null
 sudo modprobe tcp_probe port=$1 full=1
 sleep 1
-echo "$(date +%s.%N) BOOT $(grep 'CONFIG_HZ=' /boot/config-$(uname -r))"
+echo "$(date +%s.%N) BOOT $config_hz"
 exec sudo cat /proc/net/tcpprobe
 
