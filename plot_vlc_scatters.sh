33acbb45b3870bcc81a49ddf083c59d1e40626ba
Sergio Livi <livi@i3s.unice.fr>
2015-07-08 13:59:45 +0200

    unfairness bug, minor things

--- plot_vlc_scatters.sh
+++ plot_vlc_scatters.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 python plot_vlc_scatters.py two_con_scatters_classic.png tests/constant_two_con_bipbop_200ms_200p/*_classic
+python plot_vlc_scatters.py two_con_scatters_classic-2.png tests/constant_two_con_bipbop_200ms_200p/*_classic-2
 python plot_vlc_scatters.py two_con_scatters_bba0.png tests/constant_two_con_bipbop_200ms_200p/*_bba0
 
