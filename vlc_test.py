327df402f4d7d3a2fbe9b0f6ed4b13a2d5a48ef4
Sergio Livi <livi@i3s.unice.fr>
2015-07-28 10:25:41 +0200

    current set

--- vlc_test.py
+++ vlc_test.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 	delay = '200ms'
 	buffer_size = 200
 
-	algorithms = ('classic-13', 'bba0', 'bba1')
+	algorithms = ('classic-13', 'bba0')
 	bandwidths = ('400kbit', '500kbit', '600kbit', '700kbit', '800kbit', '900kbit', '1000kbit', '1100kbit', '1200kbit', '1300kbit', '1400kbit', '1500kbit', '1600kbit', '1700kbit', '1800kbit', '1900kbit', '2000kbit', '2100kbit', '2200kbit', '2300kbit', '2400kbit', '2500kbit', '2600kbit', '2700kbit', '2800kbit', '2900kbit', '3000kbit')
 	collection = 'constant_single_bbb8_{0}_{1}p'.format(delay, buffer_size)
 	for curl in (False, True):
@@ -28,7 +28,7 @@ if __name__ == "__main__":
 	algorithms = ('classic-13', 'bba0')
 	bandwidths = ('800kbit', '1000kbit', '1200kbit', '1400kbit', '1600kbit', '1800kbit', '2000kbit', '2200kbit', '2400kbit', '2600kbit', '2800kbit', '3000kbit', '3200kbit', '3400kbit', '3600kbit', '3800kbit', '4000kbit', '4200kbit', '4400kbit', '4600kbit', '4800kbit', '5000kbit', '5200kbit', '5400kbit', '5600kbit', '5800kbit', '6000kbit')
 	collection = 'constant_two_con_bbb8_{0}_{1}p'.format(delay, buffer_size)
-	for curl in (False, True):
+	for curl in (True):
 		for algo in algorithms:
 			player1 = Player(delay=1, host='client0', algo=algo, curl=curl, url=bigbuckbunny8_url, kill_after=700)
 			player2 = Player(delay=1, host='client1', algo=algo, curl=curl, url=bigbuckbunny8_url, kill_after=700)
@@ -42,7 +42,7 @@ if __name__ == "__main__":
 				num += 1
 
 
-	algorithms = ('classic-2', 'bba0', 'bba1')
+	algorithms = ('classic-2', 'bba0')
 	bandwidths = ('300kbit', '400kbit', '500kbit', '600kbit', '700kbit', '800kbit', '900kbit', '1000kbit', '1100kbit', '1200kbit', '1300kbit', '1400kbit', '1500kbit', '1600kbit', '1700kbit', '1800kbit', '1900kbit', '2000kbit', '2100kbit', '2200kbit')
 	collection = 'constant_single_bipbop_{0}_{1}p'.format(delay, buffer_size)
 	for curl in (False, True):
