0408348cfdc4f1fb383f69307084c0a41993b0b5
Sergio Livi <livi@i3s.unice.fr>
2015-10-22 17:20:29 +0200

    extra space

--- code/post_run.d/tshark_dst_clients.sh
+++ code/post_run.d/tshark_dst_clients.sh
@@ -3,6 +3,6 @@
 for pcap in $RUN_PATH/*.pcap; do
 	[ -e "$pcap" ] || break
 	echo "Processing $pcap..."
-	tshark -r "$pcap" -Y 'ip.dst == 192.168.200.0/24 and tcp.len > 0' -T fields -E separator=, -e frame.time_epoch  -e ip.src -e tcp.srcport -e ip.dst -e tcp.dstport -e tcp.len > "$pcap.toclients"
+	tshark -r "$pcap" -Y 'ip.dst == 192.168.200.0/24 and tcp.len > 0' -T fields -E separator=, -e frame.time_epoch -e ip.src -e tcp.srcport -e ip.dst -e tcp.dstport -e tcp.len > "$pcap.toclients"
 done
 
