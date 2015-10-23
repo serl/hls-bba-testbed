1ee713eb4d98015b1919841448e1c1bfaf9b4d48
Sergio Livi <livi@i3s.unice.fr>
2015-10-23 16:22:09 +0200

    anti-suicide

--- analyze_vlc_QoE.sh
+++ analyze_vlc_QoE.sh
@@ -13,19 +13,6 @@ exists() {
 TESTDIR="$1"
 mkdir -p "$TESTDIR/QoE_metrics/"
 
-# BipBop
-for algo in classic-2_keepalive classic-2_keepalive_est classic-23_keepalive classic-23_keepalive_est bba0_keepalive_est bba1_keepalive_est bba2_keepalive_est bba3_keepalive_est bba3-a_keepalive_est; do
-	for num in single two_con; do
-		outfile="$TESTDIR/QoE_metrics/${num}_bipbop_200ms_200p_$algo"
-		tests=$TESTDIR/constant_${num}_bipbop_200ms_200p/c*_$algo
-		if [ ! -f "$outfile" ] && exists $tests; then
-			echo python analyze_vlc_QoE.py $outfile $tests
-		fi
-	done
-done | parallel --gnu --eta -j3
-echo "$? jobs failed."
-
-
 # Big Buck Bunny
 for algo in classic-13_keepalive classic-13_keepalive_est classic-119_keepalive classic-119_keepalive_est bba0_keepalive_est bba1_keepalive_est bba2_keepalive_est bba3_keepalive_est bba3-a_keepalive_est; do
 	for delaybuffer in 100ms_200p 200ms_200p 400ms_200p 200ms_100%p 200ms_50%p 200ms_25%p 200ms_10%p; do
@@ -39,6 +26,20 @@ for algo in classic-13_keepalive classic-13_keepalive_est classic-119_keepalive 
 	done
 done | parallel --gnu --eta -j5
 echo "$? jobs failed."
+
+
+# BipBop
+for algo in classic-2_keepalive classic-2_keepalive_est classic-23_keepalive classic-23_keepalive_est bba0_keepalive_est bba1_keepalive_est bba2_keepalive_est bba3_keepalive_est bba3-a_keepalive_est; do
+	for num in single two_con; do
+		outfile="$TESTDIR/QoE_metrics/${num}_bipbop_200ms_200p_$algo"
+		tests=$TESTDIR/constant_${num}_bipbop_200ms_200p/c*_$algo
+		if [ ! -f "$outfile" ] && exists $tests; then
+			echo python analyze_vlc_QoE.py $outfile $tests
+		fi
+	done
+done | parallel --gnu --eta -j1
+echo "$? jobs failed."
+
 
 single_files=$TESTDIR/QoE_metrics/single_bipbop_200ms_200p_* $TESTDIR/QoE_metrics/single_bbb_100ms_200p_* $TESTDIR/QoE_metrics/single_bbb_200ms_200p_* $TESTDIR/QoE_metrics/single_bbb_400ms_200p_* $TESTDIR/QoE_metrics/single_bbb_200ms_100%p_* $TESTDIR/QoE_metrics/single_bbb_200ms_50%p_* $TESTDIR/QoE_metrics/single_bbb_200ms_25%p_* $TESTDIR/QoE_metrics/single_bbb_200ms_10%p_*
 if exists $single_files; then
