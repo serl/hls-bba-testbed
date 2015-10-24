23a910c71d7cb77cb027b88db49ee522cdea1fb2
Sergio Livi <livi@i3s.unice.fr>
2015-10-24 19:50:08 +0200

    trivial error

--- analyze_vlc_QoE.sh
+++ analyze_vlc_QoE.sh
@@ -41,14 +41,14 @@ done | parallel --gnu --eta -j1
 echo "$? jobs failed."
 
 
-single_files=$TESTDIR/QoE_metrics/single_bipbop_200ms_200p_* $TESTDIR/QoE_metrics/single_bbb_100ms_200p_* $TESTDIR/QoE_metrics/single_bbb_200ms_200p_* $TESTDIR/QoE_metrics/single_bbb_400ms_200p_* $TESTDIR/QoE_metrics/single_bbb_200ms_100%p_* $TESTDIR/QoE_metrics/single_bbb_200ms_50%p_* $TESTDIR/QoE_metrics/single_bbb_200ms_25%p_* $TESTDIR/QoE_metrics/single_bbb_200ms_10%p_*
+single_files="$TESTDIR/QoE_metrics/single_bipbop_200ms_200p_* $TESTDIR/QoE_metrics/single_bbb_100ms_200p_* $TESTDIR/QoE_metrics/single_bbb_200ms_200p_* $TESTDIR/QoE_metrics/single_bbb_400ms_200p_* $TESTDIR/QoE_metrics/single_bbb_200ms_100%p_* $TESTDIR/QoE_metrics/single_bbb_200ms_50%p_* $TESTDIR/QoE_metrics/single_bbb_200ms_25%p_* $TESTDIR/QoE_metrics/single_bbb_200ms_10%p_*"
 if exists $single_files; then
 	echo "label,rebuffering_ratio,,avg_bitrate,,avg_relative_bitrate,,avg_quality_level,,instability,,link_utilization,,avg_router_queue_len,,avg_relative_router_queue_len,,avg_relative_rtt" > $TESTDIR/QoE_metrics/single_all
 	cat $single_files >> $TESTDIR/QoE_metrics/single_all
 	sed -i '/^$/d' $TESTDIR/QoE_metrics/single_all
 fi
 
-two_con_files=$TESTDIR/QoE_metrics/two_con_bipbop_200ms_200p_* $TESTDIR/QoE_metrics/two_con_bbb_100ms_200p_* $TESTDIR/QoE_metrics/two_con_bbb_200ms_200p_* $TESTDIR/QoE_metrics/two_con_bbb_400ms_200p_* $TESTDIR/QoE_metrics/two_con_bbb_200ms_100%p_* $TESTDIR/QoE_metrics/two_con_bbb_200ms_50%p_* $TESTDIR/QoE_metrics/two_con_bbb_200ms_25%p_* $TESTDIR/QoE_metrics/two_con_bbb_200ms_10%p_*
+two_con_files="$TESTDIR/QoE_metrics/two_con_bipbop_200ms_200p_* $TESTDIR/QoE_metrics/two_con_bbb_100ms_200p_* $TESTDIR/QoE_metrics/two_con_bbb_200ms_200p_* $TESTDIR/QoE_metrics/two_con_bbb_400ms_200p_* $TESTDIR/QoE_metrics/two_con_bbb_200ms_100%p_* $TESTDIR/QoE_metrics/two_con_bbb_200ms_50%p_* $TESTDIR/QoE_metrics/two_con_bbb_200ms_25%p_* $TESTDIR/QoE_metrics/two_con_bbb_200ms_10%p_*"
 if exists $two_con_files; then
 	echo "label,rebuffering_ratio,,avg_bitrate,,avg_relative_bitrate,,avg_quality_level,,instability,,link_utilization,,avg_router_queue_len,,avg_relative_router_queue_len,,avg_relative_rtt,,general_unfairness,,quality_unfairness" > $TESTDIR/QoE_metrics/two_con_all
 	cat $two_con_files >> $TESTDIR/QoE_metrics/two_con_all
