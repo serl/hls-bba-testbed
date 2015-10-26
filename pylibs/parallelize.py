12719d974ca33c5eb3204a9f4ccef19726f59c74
Sergio Livi <livi@i3s.unice.fr>
2015-10-26 10:49:22 +0100

    now really working with exceptions

--- pylibs/parallelize.py
+++ pylibs/parallelize.py
@@ -1,5 +1,5 @@
 from multiprocessing import Process, Pipe
-import time
+import sys, time
 
 class AsyncFunction(object):
 	def __init__(self, fn, args=(), kwargs={}, return_obj=None, return_attr=None):
@@ -18,7 +18,8 @@ class AsyncFunction(object):
 		try:
 			result = self.fn(*self.args, **self.kwargs)
 		except Exception as e:
-			exception = e
+			import traceback
+			exception = "".join(traceback.format_exception(*sys.exc_info()))
 		finally:
 			conn.send((result, exception))
 			conn.close()
@@ -32,7 +33,7 @@ class AsyncFunction(object):
 		if self._conn.poll(timeout):
 			self.result, exception = self._conn.recv()
 			if exception is not None:
-				raise exception
+				raise InnerException(exception)
 			if self.return_obj is not None and self.return_attr is not None:
 				self.return_obj.__dict__[self.return_attr] = self.result
 			self._process.join()
@@ -42,6 +43,8 @@ class AsyncFunction(object):
 	def terminate(self):
 		if self._process.is_alive():
 			self._process.terminate()
+
+class InnerException(Exception): pass
 
 class Parallelize(object):
 	def __init__(self, collection, **kwargs):
@@ -68,20 +71,30 @@ class Parallelize(object):
 if __name__ == '__main__':
 	def sleepy(t):
 		time.sleep(t)
+		if t == 2:
+			raise Exception('ciao!')
 		return "after {0}s".format(t)
 
 	class resultclass:
 		pass
 
 	result=resultclass()
-
 	coll = [
 		{'args': (3,), 'return_attr': 'a'},
 		{'args': (1,), 'return_attr': 'b'}
 	]
 	p = Parallelize(coll, fn=sleepy, return_obj=result)
 	p.run()
+	print(result.__dict__)
+	print(p.results)
 
+	result=resultclass()
+	coll = [
+		{'args': (300,), 'return_attr': 'a'},
+		{'args': (2,), 'return_attr': 'b'}
+	]
+	p = Parallelize(coll, fn=sleepy, return_obj=result)
+	p.run()
 	print(result.__dict__)
 	print(p.results)
 
