5635c5c201f2c86e4001d49ed71a09aa25edcf64
Sergio Livi <livi@i3s.unice.fr>
2016-02-09 19:15:51 +0100

    ssh security changed, now working again

--- scripts/setup_ssh_keys.sh
+++ scripts/setup_ssh_keys.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ssh-keygen -t rsa -f /vagrant/scripts/ssh/id_rsa -q -N ''
 cp /vagrant/scripts/ssh/id_rsa* $HOME/.ssh
+chmod 600 $HOME/.ssh/id_rsa
 cat /vagrant/scripts/ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-
