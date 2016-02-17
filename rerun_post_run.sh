3d55545210760e166f198a7193e59d524d3e92da
Sergio Livi <livi@i3s.unice.fr>
2016-02-17 14:31:37 +0100

    added rerun_post_run

---
+++ rerun_post_run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -z "$1" -o ! -e "$1" ]; then
+	echo "Usage: $0 <rundir|run.tar.gz> [rundir|run.tar.gz...]"
+	exit 1
+fi
+
+vagrant up server
+
+if [ $# -eq 1 ]; then
+	vagrant ssh server --command "/vagrant/code/post_run.sh /vagrant/$1"
+	exit $?
+fi
+
+for rundir in "$@"; do echo /vagrant/$rundir; done |
+vagrant ssh server -- bash -c "'
+	parallel --gnu -j2 --eta /vagrant/code/post_run.sh
+	echo -e "\\\\n\$? jobs failed."
+'"
